02f87414b4fbcbd66d97511e344bee65224854db
Darshan-Jain-07 <166013023+Darshan-Jain-07@users.noreply.github.com>
2024-04-21 01:23:49 +0530

    Update SQLdb.js

--- backend/SQLdb.js
+++ backend/SQLdb.js
@@ -1,9 +1,9 @@
 const { createConnection } = require('mysql2');
 
 const connection = createConnection({
-    host: "localhost",
-    user: "root",
-    database: "aerotech",
+    host: "infinityfree.com",
+    user: "if0_34856207",
+    database: "if0_34856207_aerotech",
 })
 
 module.exports = connection;
